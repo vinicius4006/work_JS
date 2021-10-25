d3699188391e7686753c242589a47ddf88d5545c
Vinicius Silva <vfranca477@gmail.com>
2021-10-25 16:50:27 -0300

    commit

---
+++ UdemyJS/Test Topics JS/DeckOfCards.js
@@ -0,0 +1,75 @@
+/* 1
+function makeDeck(){
+const deck = []
+const suits = ["hearts","diamonds","spades","clubs"]
+const values = "A,2,3,4,5,6,7,8,9,10,J,Q,K"
+for (let value of values.split(","))
+    for (let suit of suits)
+        deck.push({
+            value,
+            suit
+        })
+        return deck
+    
+}
+function drawCard(deck){
+    return deck.pop()
+}
+const myDeck = makeDeck()
+console.log(myDeck.length)
+const card1 = drawCard(myDeck)
+console.log(card1)
+*/
+//          2 
+const myDeck = {
+    deck: [],
+    drawCards: [],
+    suits: ["hearts","diamonds","spades","clubs"],
+    values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+    initializeDeck(){
+        const {
+            suits,
+            values,
+            deck
+        } = this
+        for (let value of values.split(","))
+        for (let suit of suits)
+           deck.push({
+               value,
+               suit
+           })
+    },
+    drawCard(){
+        const card = this.deck.pop()
+        this.drawCards.push(card)
+        return card
+    },
+    drawMultiple(numCards){
+        const cards = []
+        for(let i = 0; i< numCards;i++)
+            cards.push(this.drawCard())
+                return cards
+    },
+    shuffle(){
+        const {
+            deck
+        } = this
+        
+        for (let i = deck.length - 1; i > 0; i--){
+            let j =  Math.floor(Math.random() * (i + 1));
+
+             [deck[i], deck[j]] = [deck[j], deck[i]]
+         }
+        
+    }
+     
+}
+//const firstDeck = initializeDeck()
+myDeck.initializeDeck()
+myDeck.shuffle()
+const h1 =  myDeck.drawMultiple(5)
+const h2 =  myDeck.drawMultiple(5)
+const h3 =  myDeck.drawMultiple(5)
+console.log(h1)
+
+
