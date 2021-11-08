28d4e9649bc09ec015c9ace08302b2f9b053c5ae
Vinicius Silva <vfranca477@gmail.com>
2021-11-08 10:50:56 -0300

    commit

--- UdemyJS/Test Topics JS/webpage/app.js
+++ UdemyJS/Test Topics JS/webpage/app.js
@@ -1,1 +1,8 @@
 //querySelector("section ul li.special") exemplo
+const allLis = document.querySelectorAll("li")
+
+for (let item in allLis)
+    allLis[item].innerText = "WE ARE THE CHAMPIONS!"
+
+for (let elem of allLis)
+    elem.innerHTML = "We are <b>THE CHAMPIONS</br>"
